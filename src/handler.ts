f34617d613ed34bbb94f1301a61bf8992e632297
Dustin Schau <dustinschau@gmail.com>
2017-12-17 09:47:11 -0600

    feat: add in ability to call with url-encoded params

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,15 @@
+import * as qs from 'querystring';
 import { send } from './util/send';
 
 const isWarmUp = ev => ev.source === 'serverless-plugin-warmup';
+
+const parseBody = body => {
+  try {
+    return JSON.parse(body);
+  } catch (e) {
+    return qs.parse(body);
+  }
+};
 
 export async function email(ev, context, callback) {
   if (isWarmUp(ev)) {
@@ -8,7 +17,9 @@ export async function email(ev, context, callback) {
     return callback(null, 'Lambda is warm!');
   }
 
-  const response = await send(ev.body).catch(e => callback(e));
+  const body = parseBody(ev.body);
+
+  const response = await send(body).catch(e => callback(e));
 
   return callback(null, {
     statusCode: 200,
